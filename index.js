7657a50ce721a4d02a7084b274cf650fd73cfa42
BrianKohnen <briankohnen@yahoo.com>
2019-08-24 11:42:16 -0500

    bk--game can correctly track wins and generate a new word for the user. working on decrementing user's guesses left on incorrect guesses

--- index.js
+++ index.js
@@ -1,15 +1,63 @@
-var Word = require("./Word");
+const Word = require("./Word");
+const prompt = require("prompt");
 
 let fruitsWords = ["strawberry", "grape", "kiwi", "apple", "starfruit",
                     "pineapple", "banana", "gooseberry", "kumquat", "plum",
                     "lemon", "honeydew", "peach", "cantaloupe", "fig", "date",
                     "papaya", "orange", "apricot", "huckleberry", "pomegranate",
                     "lime", "tangerine", "watermelon", "cherry", "dragonfruit",
-                    "clementine", "nectarine", "mango", "guava", "pear"]
+                    "clementine", "nectarine", "mango", "guava", "pear"];
 
-let randomFroot = new Word(fruitsWords[Math.ceil((Math.random() * fruitsWords.length))]);
+let wins = 0;
 
-randomFroot.letterify();
+function newWord() {
 
-console.log(randomFroot);
+    console.log("YOUR WINS : " + wins);
+
+    let randomFroot = new Word(fruitsWords[Math.ceil((Math.random() * fruitsWords.length))]);
+
+    let guessesLeft = 10;
+
+    randomFroot.letterify();
+
+    console.log(randomFroot.toString());
+
+    function askForGuess() {
+
+        console.log("GUESSES LEFT : " + guessesLeft);
+
+        if (guessesLeft === 0) {
+
+            throw console.log("Sorry, you lost");
+
+        }
+
+        if (!randomFroot.toString().includes("_")) {
+
+            wins++;
+            newWord();
+
+        } else {
+            
+            prompt.start();
+
+            prompt.get(["Guess"], function (err, result) {
+                
+                randomFroot.checkGuess(result.Guess);
+
+                console.log(randomFroot.toString());
+
+                askForGuess();
+
+
+            });
+
+        }
+    }
+
+    askForGuess();
+};
+
+newWord();
+
 
